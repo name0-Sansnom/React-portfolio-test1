4793a48b8f0c7853d129827f1f08457a6eb19ddd
name0-Sansnom <67804626+name0-Sansnom@users.noreply.github.com>
2023-11-20 09:01:36 +0000

    Portolio Updata

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../asset/portfolio2.jpg";
-import IMG2 from "../../asset/portfolio3.jpg";
-import IMG3 from "../../asset/portfolio4.jpg";
-import IMG4 from "../../asset/portfolio5.png";
-import IMG5 from "../../asset/portfolio6.jpg";
+import IMG1 from "../../asset/portfolio8.jpg";
+import IMG2 from "../../asset/portfolio9.jpg";
+import IMG3 from "../../asset/portfolio7.jpg";
+import IMG4 from "../../asset/portfolio10.png";
+import IMG5 from "../../asset/portfolio11.jpg";
 import IMG6 from "../../asset/portfolio1.jpg";
 
 const portfolioData = [
   {
     id: 1,
-    title: "my portfolio",
+    title: "Emanpay",
     image: IMG1 ,
-    github_btn: "https://github.com/name0-Sansnom?tab=repositories",
-    demo_btn: "https://github.com/name0-Sansnom"
+    github_btn: "https://emanpay.netlify.app",
+    demo_btn: "https://emanpay.netlify.app"
   },
   {
     id: 2,
-    title: "my portfolio",
+    title: "CloudAir",
     image: IMG2 ,
-    github_btn: "https://github.com/name0-Sansnom?tab=repositories",
-    demo_btn: "https://github.com/name0-Sansnom"
+    github_btn: "https://cloudair.netlify.app",
+    demo_btn: "https://cloudair.netlify.app"
   },
   {
     id: 3,
-    title: "my portfolio",
+    title: "Al-Hikma",
     image: IMG3 ,
-    github_btn: "https://github.com/name0-Sansnom?tab=repositories",
-    demo_btn: "https://github.com/name0-Sansnom"
+    github_btn: "https://alhikmaready.netlify.app",
+    demo_btn: "https://alhikmaready.netlify.app"
   },
   {
     id: 4,
-    title: "my portfolio",
+    title: "NGO",
     image: IMG4 ,
-    github_btn: "https://github.com/name0-Sansnom?tab=repositories",
-    demo_btn: "https://github.com/name0-Sansnom"
+    github_btn: "https://zongo-ngo.netlify.app",
+    demo_btn: "https://zongo-ngo.netlify.app"
   },
   {
     id: 5,
-    title: "my portfolio",
+    title: "Restaurant Menu",
     image: IMG5 ,
     github_btn: "https://github.com/name0-Sansnom?tab=repositories",
     demo_btn: "https://github.com/name0-Sansnom"
